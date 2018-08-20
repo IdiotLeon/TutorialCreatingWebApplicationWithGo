02e38c231df9461ed730aee19da8754a91005ed7
Leon <yanglyu.leon.7@gmail.com>
2018-08-20 08:54:35 -0700

    demo: Looping

--- src/github.com/lss/webapp/viewmodel/shop.go
+++ src/github.com/lss/webapp/viewmodel/shop.go
@@ -7,10 +7,11 @@ type Shop struct {
 }
 
 type Category struct {
-	URL         string
-	ImageURL    string
-	Title       string
-	Description string
+	URL           string
+	ImageURL      string
+	Title         string
+	Description   string
+	IsOrientRight bool
 }
 
 func NewShop() Shop {
@@ -25,6 +26,7 @@ func NewShop() Shop {
 		Description: `Explore our wide assortment of juices and mixes expected by today's
 		lemonade stand clientelle. Now featuring a full line of organic juices that are guaranceed
 		to be obtained from trees that have never been treated with pesticide or artificial fertilizers.`,
+		IsOrientRight: false,
 	}
 	supplyCategory := Category{
 		URL:      ".",
@@ -32,6 +34,7 @@ func NewShop() Shop {
 		Title:    "Cups, Straws, and Other Supplies",
 		Description: `From paper cups to bio-degradable plastic to straws and napkins,
 		LSS is your source for the sundries that keep you stand running smoothl.`,
+		IsOrientRight: true,
 	}
 	advertiseCategory := Category{
 		URL:      ".",
@@ -40,6 +43,7 @@ func NewShop() Shop {
 		Description: `Sure, you could just wait for people to find your stand along
 		the side of the road, but if you want to take it to the next level, our premium
 		line of advertising supplies.`,
+		IsOrientRight: false,
 	}
 	result.Categories = []Category{juiceCategory, supplyCategory, advertiseCategory}
 	return result
